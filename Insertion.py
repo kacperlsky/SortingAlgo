9d81b026e4ed388de849445ce756f9dad6edad29
Kacperlsky <nothingisimpossiblechannel@gmail.com>
2020-04-28 20:16:36 +0200

    minor changes selection&Insertion Sort

--- Insertion.py
+++ Insertion.py
@@ -10,37 +10,47 @@ def mergeSort(array):
     return 0
 
 def selectionSort(array):
-    newr= list()
+    newArray= list()
+
     for i in range(len(array)):
         minval = min(array)
-        
-        newr.append(minval)
-        indo = array.index(minval)
-        del(array[indo])
-        print("%i stage", newr)
+        newArray.append(minval)
+        indexMinVal = array.index(minval)
+        del(array[indexMinVal])
+        print("{} stage".format(i+1), newArray + array)
+    print(newArray)
     
-    print(newr)
+    """
+    basic version using only one array instead of rewritting 2 arrays
+
+    for i in range(len(array)): 
+        minId = i 
+        for j in range(i+1, len(array)): 
+            if array[minId] > array[j]: 
+                minId = j 
+                 
+        array[i], array[minId] = array[minId], array[i] 
+    """
 
 def quickSort(array):
     return 0
 
 def insertionSort(array):
-    for index in range(1, len(array)):
-        currentValue = array[index]
-        currentPosition = index
+    for i in range(1, len(array)):
+        Val = array[i]
+        Pos = i
 
-        while array[currentPosition - 1] > currentValue and currentPosition > 0:
-            array[currentPosition] = array[currentPosition -1]
-            currentPosition = currentPosition - 1
-        array[currentPosition] = currentValue
-        print(array)
-    print(array)
+        while array[Pos - 1] > Val and Pos > 0:
+            array[Pos] = array[Pos -1]
+            Pos = Pos - 1
+        array[Pos] = Val
+        print("{} Stage".format(i), array)
+    print("Final array", array)
 
 
 
 if __name__ == "__main__":
     sortingChoice = int(input("Hey, please enter the number and choose one of the following sorting algorithms: \n 1-Insertion Sort\n 2-Merge Sort\n 3-Quick Sort\n 4-Selection Sort\n Your Choice: "))
-    
     if sortingChoice == 1:
         array = array_input()
         insertionSort(array)
